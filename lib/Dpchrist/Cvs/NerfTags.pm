a0933a384780664ff00df3bfed45ddf8ae5d869c
David Paul Christensen <dpchrist@holgerdanske.com>
2010-11-27 04:51:54 +0000

    Import of DPCHRIST/Dpchrist-Cvs-NerfTags-1.009 from CPAN.
    gitpan-cpan-distribution: Dpchrist-Cvs-NerfTags
    gitpan-cpan-version:      1.009
    gitpan-cpan-path:         DPCHRIST/Dpchrist-Cvs-NerfTags-1.009.tar.gz
    gitpan-cpan-author:       DPCHRIST
    gitpan-cpan-maturity:     released

--- lib/Dpchrist/Cvs/NerfTags.pm
+++ lib/Dpchrist/Cvs/NerfTags.pm
@@ -1,5 +1,5 @@
 #######################################################################
-# $Id: NerfTags.pm,v 1.8 2010-11-26 20:53:09 dpchrist Exp $
+# $Id: NerfTags.pm,v 1.9 2010-11-27 03:37:57 dpchrist Exp $
 #######################################################################
 # package:
 #----------------------------------------------------------------------
@@ -21,7 +21,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw();
 
-our $VERSION = sprintf "%d.%03d", q$Revision: 1.8 $ =~ /(\d+)/g;
+our $VERSION = sprintf "%d.%03d", q$Revision: 1.9 $ =~ /(\d+)/g;
 
 #######################################################################
 # uses:
@@ -61,7 +61,7 @@ Dpchrist::Cvs::NerfTags - disable RCS/CVS tags in working files
 
 =head1 DESCRIPTION
 
-This documentation describes module revision $Revision: 1.8 $.
+This documentation describes module revision $Revision: 1.9 $.
 
 
 This is alpha test level software
@@ -231,17 +231,32 @@ See 'perldoc Export' for everything in between.
 
 =head1 INSTALLATION
 
-    perl Makefile.PL
-    make
-    make test
-    make install
+Old school:
+
+    $ perl Makefile.PL
+    $ make
+    $ make test
+    $ make install
+
+Minimal:
+
+    $ cpan Dpchrist::Cvs::Nerftags
+    
+Complete:
+
+    $ cpan Bundle::Dpchrist
+
+The following warning may be safely ignored:
+
+    Can't locate Dpchrist/Module/MakefilePL.pm in @INC (@INC contains: /
+    etc/perl /usr/local/lib/perl/5.10.0 /usr/local/share/perl/5.10.0 /us
+    r/lib/perl5 /usr/share/perl5 /usr/lib/perl/5.10 /usr/share/perl/5.10
+    /usr/local/lib/site_perl .) at Makefile.PL line 22.
 
 
-=head1 DEPENDENCIES
+=head2 PREREQUISITES
 
-    Perl 5.10
-    Dpchrist::Debug
-    Dpchrist::Module
+See Makefile.PL in the source distribution root directory.
 
 
 =head1 SEE ALSO
